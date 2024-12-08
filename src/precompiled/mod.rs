08891e6627f8de37d62d8dc18881cb26a11c8a16
kungfuflex <judoflexchop@gmail.com>
2024-12-08 17:39:51 +0000

    update

--- src/precompiled/mod.rs
+++ src/precompiled/mod.rs
@@ -4,3 +4,4 @@ pub mod alkanes_std_genesis_alkane_luckycoin_build;
 pub mod alkanes_std_genesis_alkane_mainnet_build;
 pub mod alkanes_std_genesis_alkane_fractal_build;
 pub mod alkanes_std_genesis_alkane_dogecoin_build;
+pub mod alkanes_std_auth_token_build;
