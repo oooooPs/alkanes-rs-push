b85fd9af23b7a3869ede8dcdb3a43c6549ff3bdb
kungfuflex <judoflexchop@gmail.com>
2024-12-29 17:18:23 +0000

    add declare_alkane macro

--- crates/alkanes-runtime/src/lib.rs
+++ crates/alkanes-runtime/src/lib.rs
@@ -7,3 +7,17 @@ pub mod stdio;
 pub mod storage;
 pub mod token;
 pub use crate::stdio::stdout;
+
+use metashrew_support::{compat::{to_arraybuffer_layout, to_passback_ptr}};
+
+#[macro_export]
+macro_rules! declare_alkane {
+    ($struct_name:ident) => {
+        #[no_mangle]
+        pub extern "C" fn __execute() -> i32 {
+            let mut response = to_arraybuffer_layout(&$struct_name::default().run());
+            to_passback_ptr(&mut response)
+        }
+    };
+}
+
