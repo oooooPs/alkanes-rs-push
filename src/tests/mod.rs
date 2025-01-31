ab6978e8838b7a8c32051cb8ef0dff25623fc775
kungfuflex <judoflexchop@gmail.com>
2025-01-31 20:57:45 +0000

    merge

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -5,21 +5,17 @@ pub mod std;
 #[cfg(test)]
 pub mod utils;
 //pub mod index_alkanes;
-/*
 #[cfg(test)]
 //pub mod address;
 #[cfg(test)]
 pub mod alkane;
 #[cfg(test)]
 pub mod auth_token;
-*/
 #[cfg(test)]
 pub mod crash;
-/*
 #[cfg(test)]
 pub mod genesis;
 #[cfg(test)]
 pub mod networks;
 #[cfg(test)]
 pub mod serialization;
-*/
