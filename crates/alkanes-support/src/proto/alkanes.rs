f253de0e83e992af372f25d2032af9f51ee6e67c
kungfuflex <judoflexchop@gmail.com>
2025-03-13 21:48:37 -0500

    update

--- crates/alkanes-support/src/proto/alkanes.rs
+++ crates/alkanes-support/src/proto/alkanes.rs
@@ -3445,6 +3445,129 @@ impl ::protobuf::reflect::ProtobufValue for TraceBlockResponse {
     type RuntimeType = ::protobuf::reflect::rt::RuntimeTypeMessage<Self>;
 }
 
+// @@protoc_insertion_point(message:alkanes.BytecodeRequest)
+#[derive(PartialEq,Clone,Default,Debug)]
+pub struct BytecodeRequest {
+    // message fields
+    // @@protoc_insertion_point(field:alkanes.BytecodeRequest.id)
+    pub id: ::protobuf::MessageField<AlkaneId>,
+    // special fields
+    // @@protoc_insertion_point(special_field:alkanes.BytecodeRequest.special_fields)
+    pub special_fields: ::protobuf::SpecialFields,
+}
+
+impl<'a> ::std::default::Default for &'a BytecodeRequest {
+    fn default() -> &'a BytecodeRequest {
+        <BytecodeRequest as ::protobuf::Message>::default_instance()
+    }
+}
+
+impl BytecodeRequest {
+    pub fn new() -> BytecodeRequest {
+        ::std::default::Default::default()
+    }
+
+    fn generated_message_descriptor_data() -> ::protobuf::reflect::GeneratedMessageDescriptorData {
+        let mut fields = ::std::vec::Vec::with_capacity(1);
+        let mut oneofs = ::std::vec::Vec::with_capacity(0);
+        fields.push(::protobuf::reflect::rt::v2::make_message_field_accessor::<_, AlkaneId>(
+            "id",
+            |m: &BytecodeRequest| { &m.id },
+            |m: &mut BytecodeRequest| { &mut m.id },
+        ));
+        ::protobuf::reflect::GeneratedMessageDescriptorData::new_2::<BytecodeRequest>(
+            "BytecodeRequest",
+            fields,
+            oneofs,
+        )
+    }
+}
+
+impl ::protobuf::Message for BytecodeRequest {
+    const NAME: &'static str = "BytecodeRequest";
+
+    fn is_initialized(&self) -> bool {
+        true
+    }
+
+    fn merge_from(&mut self, is: &mut ::protobuf::CodedInputStream<'_>) -> ::protobuf::Result<()> {
+        while let Some(tag) = is.read_raw_tag_or_eof()? {
+            match tag {
+                10 => {
+                    ::protobuf::rt::read_singular_message_into_field(is, &mut self.id)?;
+                },
+                tag => {
+                    ::protobuf::rt::read_unknown_or_skip_group(tag, is, self.special_fields.mut_unknown_fields())?;
+                },
+            };
+        }
+        ::std::result::Result::Ok(())
+    }
+
+    // Compute sizes of nested messages
+    #[allow(unused_variables)]
+    fn compute_size(&self) -> u64 {
+        let mut my_size = 0;
+        if let Some(v) = self.id.as_ref() {
+            let len = v.compute_size();
+            my_size += 1 + ::protobuf::rt::compute_raw_varint64_size(len) + len;
+        }
+        my_size += ::protobuf::rt::unknown_fields_size(self.special_fields.unknown_fields());
+        self.special_fields.cached_size().set(my_size as u32);
+        my_size
+    }
+
+    fn write_to_with_cached_sizes(&self, os: &mut ::protobuf::CodedOutputStream<'_>) -> ::protobuf::Result<()> {
+        if let Some(v) = self.id.as_ref() {
+            ::protobuf::rt::write_message_field_with_cached_size(1, v, os)?;
+        }
+        os.write_unknown_fields(self.special_fields.unknown_fields())?;
+        ::std::result::Result::Ok(())
+    }
+
+    fn special_fields(&self) -> &::protobuf::SpecialFields {
+        &self.special_fields
+    }
+
+    fn mut_special_fields(&mut self) -> &mut ::protobuf::SpecialFields {
+        &mut self.special_fields
+    }
+
+    fn new() -> BytecodeRequest {
+        BytecodeRequest::new()
+    }
+
+    fn clear(&mut self) {
+        self.id.clear();
+        self.special_fields.clear();
+    }
+
+    fn default_instance() -> &'static BytecodeRequest {
+        static instance: BytecodeRequest = BytecodeRequest {
+            id: ::protobuf::MessageField::none(),
+            special_fields: ::protobuf::SpecialFields::new(),
+        };
+        &instance
+    }
+}
+
+impl ::protobuf::MessageFull for BytecodeRequest {
+    fn descriptor() -> ::protobuf::reflect::MessageDescriptor {
+        static descriptor: ::protobuf::rt::Lazy<::protobuf::reflect::MessageDescriptor> = ::protobuf::rt::Lazy::new();
+        descriptor.get(|| file_descriptor().message_by_package_relative_name("BytecodeRequest").unwrap()).clone()
+    }
+}
+
+impl ::std::fmt::Display for BytecodeRequest {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+        ::protobuf::text_format::fmt(self, f)
+    }
+}
+
+impl ::protobuf::reflect::ProtobufValue for BytecodeRequest {
+    type RuntimeType = ::protobuf::reflect::rt::RuntimeTypeMessage<Self>;
+}
+
 #[derive(Clone,Copy,PartialEq,Eq,Debug,Hash)]
 // @@protoc_insertion_point(enum:alkanes.AlkanesTraceCallType)
 pub enum AlkanesTraceCallType {
@@ -3636,10 +3759,11 @@ static file_descriptor_proto_data: &'static [u8] = b"\
     \x01(\x0b2\x15.alkanes.AlkanesTraceR\x05trace\")\n\x11TraceBlockRequest\
     \x12\x14\n\x05block\x18\x01\x20\x01(\x04R\x05block\"<\n\x12TraceBlockRes\
     ponse\x12&\n\x06traces\x18\x01\x20\x03(\x0b2\x0e.alkanes.TraceR\x06trace\
-    s*L\n\x14AlkanesTraceCallType\x12\x08\n\x04NONE\x10\0\x12\x08\n\x04CALL\
-    \x10\x01\x12\x10\n\x0cDELEGATECALL\x10\x02\x12\x0e\n\nSTATICCALL\x10\x03\
-    *2\n\x16AlkanesTraceStatusFlag\x12\x0b\n\x07SUCCESS\x10\0\x12\x0b\n\x07F\
-    AILURE\x10\x01b\x06proto3\
+    s\"4\n\x0fBytecodeRequest\x12!\n\x02id\x18\x01\x20\x01(\x0b2\x11.alkanes\
+    .AlkaneIdR\x02id*L\n\x14AlkanesTraceCallType\x12\x08\n\x04NONE\x10\0\x12\
+    \x08\n\x04CALL\x10\x01\x12\x10\n\x0cDELEGATECALL\x10\x02\x12\x0e\n\nSTAT\
+    ICCALL\x10\x03*2\n\x16AlkanesTraceStatusFlag\x12\x0b\n\x07SUCCESS\x10\0\
+    \x12\x0b\n\x07FAILURE\x10\x01b\x06proto3\
 ";
 
 /// `FileDescriptorProto` object which was a source for this generated file
@@ -3657,7 +3781,7 @@ pub fn file_descriptor() -> &'static ::protobuf::reflect::FileDescriptor {
     file_descriptor.get(|| {
         let generated_file_descriptor = generated_file_descriptor_lazy.get(|| {
             let mut deps = ::std::vec::Vec::with_capacity(0);
-            let mut messages = ::std::vec::Vec::with_capacity(22);
+            let mut messages = ::std::vec::Vec::with_capacity(23);
             messages.push(Uint128::generated_message_descriptor_data());
             messages.push(AlkaneId::generated_message_descriptor_data());
             messages.push(AlkaneTransfer::generated_message_descriptor_data());
@@ -3680,6 +3804,7 @@ pub fn file_descriptor() -> &'static ::protobuf::reflect::FileDescriptor {
             messages.push(Trace::generated_message_descriptor_data());
             messages.push(TraceBlockRequest::generated_message_descriptor_data());
             messages.push(TraceBlockResponse::generated_message_descriptor_data());
+            messages.push(BytecodeRequest::generated_message_descriptor_data());
             let mut enums = ::std::vec::Vec::with_capacity(2);
             enums.push(AlkanesTraceCallType::generated_enum_descriptor_data());
             enums.push(AlkanesTraceStatusFlag::generated_enum_descriptor_data());
